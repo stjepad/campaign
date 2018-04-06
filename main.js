f879e8ecfaecbf84705a881fc40002257a883f1a
Dejan Stjepanovic <ds2q88@gmail.com>
2018-04-06 14:50:07 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,109 @@
+const candidate = {
+    firstName: "Elizabeth",
+    lastName: "Sanger",
+    district: "District 20",
+    bio: "Liz is short for lizard. Yes, liz is of the lizard people! all hail lizard people! @_@",
+}
+
+
+const donation = Object.create({}, {
+    donationURL: {
+        enumerable: true,
+        writable: false,
+        value: "https://www.gofundme.com/2017-lizard-lounge-poetry-slam-team"
+    }
+})
+
+
+const missionStatement = Object.create({}, {
+    missionStatement: {
+        enumerable: true,
+        writable: false,
+        value: "If you don't give me money, corporations will"
+    }
+})
+
+const platform = Object.create({}, {
+    taxes: {
+        enumerable: true,
+        writable: false,
+        value: "More taxes, more infrastructure"
+    },
+    jobs: {
+        enumerable: true,
+        writable: false,
+        value: "jobs for everyone"
+    },
+    Infrastructure: {
+        enumerable: true,
+        writable: false,
+        value: "keep it from crumblin!"
+    },
+    healthcare: {
+        enumerable: true,
+        writable: false,
+        value: "Keep em sick and keep em comin"
+    },
+    crimeAndEnforcement: {
+        enumerable: true,
+        writable: false,
+        value: "Don't do the crime, don't do the time!"
+    }
+})
+
+
+const imageGallery = Object.create({}, {
+    picture1: {
+        enumerable: true,
+        writable: false,
+        value: "http://www.liveanimalslist.com/images/hover.png"
+    },
+    picture2: {
+        enumerable: true,
+        writable: false,
+        value: "http://www.slayallpest.com/wp-content/uploads/2015/11/lizard-thumb-800x416.jpg"
+    },
+    picture3: {
+        enumerable: true,
+        writable: false,
+        value: "https://upload.wikimedia.org/wikipedia/commons/3/34/Fighting_Sand_lizards_%28Lacerta_agilis%29_4295304.jpg"
+    },
+})
+
+
+const volunteerInfo = Object.create({}, {
+    firstName: {
+        enumerable: true,
+        writable: false,
+        value: "Dejan"
+    },
+    lastName: {
+        enumerable: true,
+        writable: false,
+        value: "Stjepanovic"
+    },
+    address: {
+        enumerable: true,
+        writable: false,
+        value: "432 bleepbloop street bleepbloop USA"
+    },
+    phone: {
+        enumerable: true,
+        writable: false,
+        value: "6157372637"
+    },
+    availability: {
+        enumerable: true,
+        writable: false,
+        value: "any day any time"
+    }
+})
+
+
+const voteRegistration= Object.create({}, {
+    voteURL: {
+        enumerable: true,
+        writable: false,
+        value: "https://ovr.govote.tn.gov/"
+    }
+})
